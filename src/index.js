6ff5d33229c06ac47e74157dbc8a48dca7fe207c
Tayler <taylerblevins@gmail.com>
2024-03-12 17:12:03 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    BuildToDo(event.target['new-task-description'].value);
+  });
 });
+
+function BuildToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement(`button`)
+  btn.textContent = 'x'
+  p.textContent = todo;
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p);
+}
